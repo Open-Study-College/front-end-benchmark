58addffc1fdcabbc66ec64e4b6ed9e8873d3a6de
Tom Pearce <thomas.pearce@openstudycollege.com>
2022-03-15 11:58:57 +0000

    Defining prettier config

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    useTabs: false,
+    bracketSpacing: true,
+    tabWidth: 4,
+    printWidth: 100,
+    overrides: [
+        {
+            files: "*.json",
+            options: {
+                tabWidth: 2
+            }
+        }
+    ]
+};
